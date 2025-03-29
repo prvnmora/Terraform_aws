cc72c9cd59cc984304236ca7c3a151f083875d6e
Praveen Mora <prvn.mora@gmail.com>
2025-03-29 13:52:01 +0800

    Terraform files for creating AWS infra

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami = "ami-01938df366ac2d954"
+instance_type = "t2.micro"
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "cidr" {
+    default = "10.0.0.0/16"
+}
+variable "ami" {
+    default = ""
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
